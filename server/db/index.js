8624945ba01d904c5c9ffde26d6171765ae1bf8d
Caroline Boyle <carolinerboyle@gmail.com>
2023-03-08 21:18:16 -0500

    added assocaitions

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,37 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+// Import your models
+const Coach = require("./models/coach");
+const Message = require("./models/message");
+const Payment = require("./models/payment");
+const School = require("./models/school");
+const StudentAthlete = require("./models/studentAthlete");
 
-//associations could go here!
+// Define associations
+
+// messaging associations
+// allow for querying all the messages sent & received by a particular student athlete
+StudentAthlete.hasMany(Message, { foreignKey: "sender_id" });
+StudentAthlete.hasMany(Message, { foreignKey: "recipient_id" });
+// allow for querying all the messages sent & received by a particular coach
+Coach.hasMany(Message, { foreignKey: "sender_id" });
+Coach.hasMany(Message, { foreignKey: "recipient_id" });
+
+// coach to school assocaition
+Coach.belongsTo(School, { foreignKey: "school_id" });
+
+// payment assocation
+StudentAthlete.hasMany(Payment, { foreignKey: "user_id" });
 
 module.exports = {
   db,
   models: {
     User,
+    Coach,
+    Payment,
+    School,
+    StudentAthlete,
   },
-}
+};
